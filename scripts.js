b47c3189be1205a55015b071a98f5f22c74b7a00
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2017-09-30 02:02:01 -0400

    slider done, section styles

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log('hello there');
+$('.slider').slick({});
+
+$('.slide img').css('visibility', '0');
+var slider = $('.slider');
+var sliderImg = $('.slider img');
+
+sliderImg.each(function() {
+    var imgSrc = $(this).attr('src');
+    console.log(imgSrc);
+    $(this).parent().css('backgroundColor', 'red');
+    $(this).parent().css('backgroundImage', 'url(' + imgSrc + ')');
+});
