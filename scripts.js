21bc48c1ed03dfabeaac47d8e9787cc19c5a93e2
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2017-10-19 00:43:04 -0400

    Initial release

--- scripts.js
+++ scripts.js
@@ -1,17 +1,34 @@
-console.log('hello there');
-$('.slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500
+$(document).ready(function(){
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+    
+    console.log('hello there');
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500
+    });
+    
+    $('.slide img').css('visibility', '0');
+    var slider = $('.slider');
+    var sliderImg = $('.slider img');
+    
+    sliderImg.each(function() {
+        var imgSrc = $(this).attr('src');
+        console.log(imgSrc);
+        $(this).parent().css('backgroundColor', 'red');
+        $(this).parent().css('backgroundImage', 'url(' + imgSrc + ')');
+    });
 });
 
-$('.slide img').css('visibility', '0');
-var slider = $('.slider');
-var sliderImg = $('.slider img');
+let navCtrl = $('.nav-control');
+let mobileNav = $('.mobile-nav');
 
-sliderImg.each(function() {
-    var imgSrc = $(this).attr('src');
-    console.log(imgSrc);
-    $(this).parent().css('backgroundColor', 'red');
-    $(this).parent().css('backgroundImage', 'url(' + imgSrc + ')');
+navCtrl.on('click', function() {
+    mobileNav.toggleClass('active');
 });
